b7daa5ef42f0cb431fe3589dad6a2ec20d50c041
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-16 21:38:03 +0100

    commit

--- server.js
+++ server.js
@@ -4,25 +4,29 @@ const port = 8080;
 const app = express();
 
 app.get('/', (req, res)=>{
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ddbf711210b97d882315154c0777c4ba"
-    https.get(url, (response)=>{
-        console.log(response.statusCode);
-        response.on("data",(data)=>{
-            const weatherData = JSON.parse(data);
-            console.log(weatherData);
-            var temperature = weatherData.main.temp;
-            var place = weatherData.name;
-            var weather = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            var imgUrl =  "https://openweathermap.org/img/wn/"+icon+"@2x.png"
+    res.sendFile(__dirname+"/index.html");
+    // const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ddbf711210b97d882315154c0777c4ba"
+    // https.get(url, (response)=>{
+    //     console.log(response.statusCode);
+    //     response.on("data",(data)=>{
+    //         const weatherData = JSON.parse(data);
+    //         console.log(weatherData);
+    //         var temperature = weatherData.main.temp;
+    //         var place = weatherData.name;
+    //         var weather = weatherData.weather[0].description;
+    //         const icon = weatherData.weather[0].icon;
+    //         var imgUrl =  "https://openweathermap.org/img/wn/"+icon+"@2x.png"
             
-            res.write("<p style ='font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;'>The weather in "+place+" is currently "+weather+" <p/>");
-            res.write("<h1 style='font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;'>Temperature in "+ place+" is "+temperature+"<h1/>");
-            res.write("<img src ='"+imgUrl+"'/>")
-            res.write("")
-            res.send();
-        })
-    })
+    //         res.write("<p style ='font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;'>The weather in "+place+" is currently "+weather+" <p/>");
+    //         res.write("<h1 style='font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;'>Temperature in "+ place+" is "+temperature+"<h1/>");
+    //         res.write("<img src ='"+imgUrl+"'/>")
+    //         res.write("")
+    //         res.send();
+    //     })
+    // })
+})
+app.post("/", (req, res)=>{
+    
 })
 
 app.listen(port, ()=>{
