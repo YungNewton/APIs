2328a224c54953005e5043e7e245eafe4f35a2a9
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-16 21:00:02 +0100

    commit

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const app = express();
 
 app.get('/', (req, res)=>{
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ddbf711210b97d882315154c0777c4ba"
-    res.send("Hello")
     https.get(url, (response)=>{
         console.log(response.statusCode);
         response.on("data",(data)=>{
             const weatherData = JSON.parse(data);
             console.log(weatherData);
+            var temperature = weatherData.main.temp
+            var place = weatherData.name
+            res.send("<h1 style='font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;'>Temperature in <h1/>"+ place+"<h1 style='font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;'> is <h1/>"+temperature);
         })
     })
 })
