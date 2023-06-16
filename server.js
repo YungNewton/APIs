34051e0e6491c2831ab7332510c47fbff86be705
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-16 20:50:21 +0100

    commit

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const port = 8080;
 const app = express();
 
 app.get('/', (req, res)=>{
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ddbf711210b97d882315154c0777c4ba"
     res.send("Hello")
-    http.get(url)
+    https.get(url, (response)=>{
+        console.log(response.statusCode);
+        response.on("data",(data)=>{
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+        })
+    })
 })
 
 app.listen(port, ()=>{
